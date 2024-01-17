ed30575df33711c96bf9c6b0c5ac43330912880f
shashidas <shashidas95@gmail.com>
2024-01-17 19:18:37 +0600

    calculator unit test

---
+++ scripts/calculator-ui.js
@@ -0,0 +1,29 @@
+// scripts/calculator.js
+
+function add() {
+ var num1 = parseFloat(document.getElementById('num1').value);
+ var num2 = parseFloat(document.getElementById('num2').value);
+ var result = num1 + num2;
+ document.getElementById('output').innerText = result;
+}
+
+function subtract() {
+ var num1 = parseFloat(document.getElementById('num1').value);
+ var num2 = parseFloat(document.getElementById('num2').value);
+ var result = num1 - num2;
+ document.getElementById('output').innerText = result;
+}
+
+function multiply() {
+ var num1 = parseFloat(document.getElementById('num1').value);
+ var num2 = parseFloat(document.getElementById('num2').value);
+ var result = num1 * num2;
+ document.getElementById('output').innerText = result;
+}
+
+function divide() {
+ var num1 = parseFloat(document.getElementById('num1').value);
+ var num2 = parseFloat(document.getElementById('num2').value);
+ var result = num1 / num2;
+ document.getElementById('output').innerText = result;
+}
